b72ca9037f07197d78781f2782bd2912699d9bd2
gunadermawan <gunadermawan.official@gmail.com>
2022-11-17 00:33:51 +0700

    create layout and exoplayer

--- app/src/main/java/com/gunder/loginscreen/MainActivity.kt
+++ app/src/main/java/com/gunder/loginscreen/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.gunder.loginscreen
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.ListPopupWindow.MATCH_PARENT
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.gunder.loginscreen.ui.theme.LoginScreenTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +47,127 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Login()
+                    Login(videoUri())
                 }
+            }
+        }
+    }
+
+    @SuppressLint("DiscouragedApi")
+    private fun videoUri(): Uri {
+        val rawId = resources.getIdentifier("clouds", "raw", packageName)
+        val videoUri = "android.resource://$packageName/$rawId"
+        return Uri.parse(videoUri)
+    }
+}
+
+private fun Context.buildExoPlayer(uri: Uri) = ExoPlayer.Builder(this).build().apply {
+    setMediaItem(MediaItem.fromUri(uri))
+    repeatMode = Player.REPEAT_MODE_ALL
+    playWhenReady = true
+    prepare()
+}
+
+private fun Context.buildPlayerPreview(exoPlayer: ExoPlayer) = StyledPlayerView(this).apply {
+    player = exoPlayer
+    layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+    useController = false
+    resizeMode = RESIZE_MODE_ZOOM
+}
+
+@Composable
+fun Login(videoUri: Uri) {
+    val context = LocalContext.current
+    val passwordRequester = FocusRequester()
+    val focusManager = LocalFocusManager.current
+    val exoPlayer = remember {
+        context.buildExoPlayer(videoUri)
+    }
+    DisposableEffect(
+        AndroidView(factory = { it.buildPlayerPreview(exoPlayer) },
+            modifier = Modifier.fillMaxSize())) {
+        onDispose {
+            exoPlayer.release()
+        }
+    }
+
+    ProvideWindowInsets {
+        Column(Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = null,
+                Modifier.size(80.dp), tint = Color.White)
+            TextInput(InputType.Name, keyboardActions = KeyboardActions(onNext = {
+                passwordRequester.requestFocus()
+            }))
+            TextInput(InputType.Password, keyboardActions = KeyboardActions(onDone = {
+                focusManager.clearFocus()
+            }), focusRequester = passwordRequester)
+            Button(onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                shape = MaterialTheme.shapes.small) {
+                Text(text = "SIGN IN", Modifier.padding(vertical = 8.dp))
+            }
+            Divider(color = Color.White.copy(alpha = 0.3f),
+                thickness = 1.dp,
+                modifier = Modifier.padding(top = 48.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "Don't have account?", color = Color.White)
+                TextButton(onClick = {}) {
+                    Text(text = "Sign Up")
+                }
+
             }
         }
     }
 }
 
-@Composable
-fun Login() {
+sealed class InputType(
+    val label: String,
+    val icon: ImageVector,
+    val keyboardOptions: KeyboardOptions,
+    val visualTransformation: VisualTransformation,
+) {
+    object Name : InputType(label = "username", icon = Icons.Default.Person,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        visualTransformation = VisualTransformation.None)
 
+    object Password : InputType(label = "password", icon = Icons.Default.Lock,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password),
+        visualTransformation = PasswordVisualTransformation())
+}
+
+@Composable
+fun TextInput(
+    inputType: InputType,
+    focusRequester: FocusRequester? = null,
+    keyboardActions: KeyboardActions,
+) {
+    var value by remember {
+        mutableStateOf("")
+    }
+    TextField(
+        value = value,
+        onValueChange = { value = it },
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusOrder(focusRequester ?: FocusRequester()),
+        leadingIcon = { Icon(imageVector = inputType.icon, null) },
+        label = { Text(text = inputType.label) },
+        shape = MaterialTheme.shapes.small,
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            backgroundColor = Color.White
+        ),
+        singleLine = true,
+        keyboardOptions = inputType.keyboardOptions,
+        keyboardActions = keyboardActions,
+        visualTransformation = inputType.visualTransformation)
 }
