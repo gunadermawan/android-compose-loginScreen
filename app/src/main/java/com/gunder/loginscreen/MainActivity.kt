ca1f1a8ed06f62c55ff46c0e1c4fa0f6adc404d9
gunadermawan <gunadermawan.official@gmail.com>
2022-11-16 22:42:34 +0700

    add drawable and raw files

--- app/src/main/java/com/gunder/loginscreen/MainActivity.kt
+++ app/src/main/java/com/gunder/loginscreen/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.gunder.loginscreen.ui.theme.LoginScreenTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +18,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    login()
+                    Login()
                 }
             }
         }
@@ -28,6 +26,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun login() {
+fun Login() {
 
 }
